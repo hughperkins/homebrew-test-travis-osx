75f063284307d81c85882875f752b04d51b9f14e
Hugh Perkins <hughperkins@gmail.com>
2015-11-23 08:49:05 +0800

    brew

---
+++ Formula/test.rb
@@ -0,0 +1,18 @@
+class HughTest < Formula
+  desc "test formula"
+  homepage "https://github.com/hughperkins/test-travis-osx"
+  depends_on "jpeg"
+  depends_on "cmake"
+
+  def install
+    pwd = Pathname.pwd
+    mkdir "build2" do
+      system "cmake", "-G", "Unix Makefiles", "..", "std_cmake_args"
+      system "make", "-j", "4"
+    end
+  end
+
+  test do
+    system "#{bin}/hugh_test"
+  end
+end
