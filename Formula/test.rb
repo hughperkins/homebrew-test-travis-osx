89e4e70f6e1b328156b0bd80b3ac2b9e2bb5ee75
Hugh Perkins <hughperkins@gmail.com>
2015-11-23 21:42:34 +0800

    asdf

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,6 +1,7 @@
 class Test < Formula
   desc "test formula"
   homepage "https://github.com/hughperkins/test-travis-osx"
+  url "https://github.com/hughperkins/clew/archive/1d2752564b6af98d24127cdb2d573d3f9ac98f02.tar.gz"
   depends_on "jpeg"
   depends_on "cmake"
 
